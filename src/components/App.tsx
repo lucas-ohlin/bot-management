65cf8446e5112b3b0b1ecf0968ac7be4229edaa1
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2024-05-12 02:57:59 +0200

    Added a cusom frame, cards for the body as well as some basic styiling.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
-import '../css/App.css'
+import React from 'react';
+import Frame from './Frame'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+
+import '../css/App.css';
+import '../css/Frame.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const minimizeWindow = (): void => { window.ipcRenderer.send('window-minimize') };
+  const maximizeWindow = (): void => { window.ipcRenderer.send('window-maximize') };
+  const closeWindow = (): void => { window.ipcRenderer.send('window-close') };
+
+  const navigateTo = (url: string): void => {
+    window.open(url, '_blank');
+  };
 
   return (
     <>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Frame minimizeWindow={minimizeWindow} maximizeWindow={maximizeWindow} closeWindow={closeWindow}/>
+      <div className='body-container'>
+        <div className="quick-actions-container">
+          <h2 className="quick-actions-title">Quick Actions</h2>
+          <div className="quick-actions-divider"></div>
+          <div className="quick-actions-btn-container">
+            <div className="quick-actions-left">
+              <button className="developer-portal-btn" onClick={() => navigateTo('https://discord.com/developers/applications')}>
+                <FontAwesomeIcon icon={faDiscord} /> Dev Portal
+              </button>
+              <button className="quick-action-btn">Import Bot</button>
+              <button className="quick-action-btn">Create Bot</button>
+            </div>
+            <div className="quick-actions-right">
+              <button className="developer-portal-btn" onClick={() => navigateTo('https://discord.gg/ybdyrPuEPR')}>
+                <FontAwesomeIcon icon={faDiscord} /> Discord
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="applications-container">
+          <h2 className="applications-title">Applications</h2>
+          <div className="applications-divider"></div>
+          <div className="applications-btn-container">
+            <button className="application-btn">App 1</button>
+            <button className="application-btn">App 2</button>
+            <button className="application-btn">App 3</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+
+export default App;
---
+++ src/components/Frame.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface FrameProps {
+  minimizeWindow: () => void;
+  maximizeWindow: () => void;
+  closeWindow: () => void;
+}
+
+const Frame: React.FC<FrameProps> = ({ minimizeWindow, maximizeWindow, closeWindow }) => {
+  return (
+    <div className="green-frame">
+      <div className="title-left">
+        <div className="profile-circle"></div>
+        <p>bot-management v0.0.1</p>
+      </div>
+      <div className="title-right">
+        <button onClick={minimizeWindow} title="Minimize" id="title-btn-1"><i className="fa-solid fa-compress"></i></button>
+        <button onClick={maximizeWindow} title="Maximize" id="title-btn-2"><i className="fa-solid fa-expand"></i></button>
+        <button onClick={closeWindow} title="Close" id="title-btn-3"><i className="fa-solid fa-xmark fa-lg"></i></button>
+      </div>
+    </div>
+  );
+};
+
+export default Frame;
